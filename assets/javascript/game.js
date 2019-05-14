412dab735e0c691a8a1f594c96488b6cbfc77a1f
kmj217 <kmj217@hotmail.com>
2019-05-14 19:32:15 -0400

    started JS variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+//=============================================================================
+//================================VARIABLES====================================
+
+//variable of number for user to match
+var magicNumber;
+
+//variable of user win count
+var wins = 0;
+
+//variable of user loss count
+var losses = 0;
+
+//variable of crystal count total
+var total;
+
+//variables to attach js to id
+var crystal1 = $("#crystal1");
+var crystal2 = $("#crystal2");
+var crystal3 = $("#crystal3");
+var crystal4 = $("#crystal3");
+
+//variable for attribute of crystal worth
+var crystalWorth;
+
+//=============================================================================
+});
