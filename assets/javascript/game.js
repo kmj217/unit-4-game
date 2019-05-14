7db3f24f0d858969cddf41a6777eb533e780a194
kmj217 <kmj217@hotmail.com>
2019-05-14 19:49:52 -0400

    setup reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,35 @@ var crystal3 = $("#crystal3");
 var crystal4 = $("#crystal3");
 
 //variable for attribute of crystal worth
-var crystalWorth;
+var crystalWorth = [];
+
+//=============================================================================
+//================================FUNCTIONS====================================
+
+//function to initialize the game, empty crystal value and total and append new values
+
+function reset() {
+    total = 0;
+    $("#total")
+        .empty()
+        .append(total);
+
+    magicNumber = [Math.floor(Math.random() * (129 - 19 + 1) + 19)];
+    $("#magicNumber")
+        .empty()
+        .append(magicNumber);
+    
+    crystalWorth[0] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalWorth[1] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalWorth[2] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalWorth[3] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+
+    crystal1.attr("crystalworth", crystalWorth[0]);
+    crystal2.attr("crystalworth", crystalWorth[1]);
+    crystal3.attr("crystalworth", crystalWorth[2]);
+    crystal4.attr("crystalworth", crystalWorth[3]);
+    }
+reset();
 
 //=============================================================================
 });
